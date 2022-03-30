3698a25ca1022d248183b296775f347fafce9509
Thezero <yonghu520@outlook.com>
2022-03-30 12:43:07 +0800

    fix: config

--- build/gulpfile.ts
+++ build/gulpfile.ts
@@ -3,6 +3,7 @@ import { copyFiles } from './copyfile';
 import { buildBundle } from './build';
 import { run, withTask } from './process';
 export default series(
+  withTask('update:version', () => run('pnpm run update:version')),
   withTask('clear', () => run('pnpm run clear')),
   buildBundle,
   copyFiles,
